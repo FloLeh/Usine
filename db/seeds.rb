2135499d1f5f69afbdf542ed9b94e6b18f96847f
Florian <florianlehuede@gmail.com>
2018-07-26 23:26:37 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+require 'faker'
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  a = Assembly.create(name: Faker::Hacker.noun)
+  p = Part.create(part_number: Faker::Hacker.abbreviation)
+  a.parts << p
+end
